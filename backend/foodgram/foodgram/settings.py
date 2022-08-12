1af665a409b4798222121221f00d1ee3411afc4b
Рогозов Михаил <mv.rogozov@yandex.ru>
2022-08-12 20:34:39 +0300

    wf

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -64,16 +64,16 @@ TEMPLATES = [
         },
     },
 ]
-
+# 'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'ENGINE': os.getenv('DB_ENGINEs', default='django.db.backends.postgresql_psycopg2'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
+        'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default=5432)
 
     }
