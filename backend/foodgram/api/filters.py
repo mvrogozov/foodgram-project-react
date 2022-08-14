5b5c80f28bb5970a9af1a982414c39f2411a997a
Рогозов Михаил <mv.rogozov@yandex.ru>
2022-08-14 15:53:19 +0300

    d

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,16 +1,19 @@
 from django_filters import rest_framework as rf_filters
 from recipes.models import Recipe
 from rest_framework import filters
+from users.models import User
 
 
 class IngredientSearchFilter(filters.SearchFilter):
     search_param = 'ingredient_name'
 
 
-
 class RecipeFilter(rf_filters.FilterSet):
     tags = rf_filters.AllValuesMultipleFilter(
         field_name='tags__slug',
+    )
+    author = rf_filters.ModelMultipleChoiceFilter(
+        queryset=User.objects.all()
     )
 
     class Meta:
