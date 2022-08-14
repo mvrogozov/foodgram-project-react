387e239d73212ad6f6d27cd70f616f7938bac110
Рогозов Михаил <mv.rogozov@yandex.ru>
2022-08-14 16:11:36 +0300

    id

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -94,6 +94,7 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = (
+            'id',
             'name',
             'measurement_unit'
         )
