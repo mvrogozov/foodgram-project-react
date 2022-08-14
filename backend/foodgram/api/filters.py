e0e047135a2edeb7e2705dc18518cdeda96f78af
Рогозов Михаил <mv.rogozov@yandex.ru>
2022-08-14 18:41:42 +0300

    filter

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -12,7 +12,6 @@ class IngredientFilter(rf_filters.FilterSet):
     pass
 
 
-
 class RecipeFilter(rf_filters.FilterSet):
     tags = rf_filters.AllValuesMultipleFilter(
         field_name='tags__slug',
@@ -20,6 +19,20 @@ class RecipeFilter(rf_filters.FilterSet):
     author = rf_filters.ModelMultipleChoiceFilter(
         queryset=User.objects.all()
     )
+    is_favorited = rf_filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = rf_filters.BooleanFilter(
+        method='filter_is_in_shopping_cart'
+    )
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(favorite__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
 
     class Meta:
         model = Recipe
