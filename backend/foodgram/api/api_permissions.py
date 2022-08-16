37f5476c1b0be81a32c47657b9adf161c4e3440a
Рогозов Михаил <mv.rogozov@yandex.ru>
2022-08-16 15:39:35 +0300

    d

--- backend/foodgram/api/api_permissions.py
+++ backend/foodgram/api/api_permissions.py
@@ -4,7 +4,10 @@ from rest_framework import permissions
 class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, obj):
-        return request.method in permissions.SAFE_METHODS
+        return (
+            request.method in permissions.SAFE_METHODS
+            or not request.user.is_anonymous
+        )
 
     def has_object_permission(self, request, view, obj):
         if request.user.is_anonymous:
